c00a07d968a21acf3ab73ecc6bc58142599613f9
Miguel Martins <miguelarbmartins@gmail.com>
2026-05-31 08:53:37 +0100

    Added sound when picking up a SparePart

--- Assets/Game/Scripts/Entities/SpareParts.cs
+++ Assets/Game/Scripts/Entities/SpareParts.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class SpareParts : MonoBehaviour
 {
     [SerializeField] private GameObject managerObject;
+    [SerializeField] private AudioClip sound;
+    [SerializeField] private AudioSource audioSource;
     private CollectableManager manager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +22,7 @@ public class SpareParts : MonoBehaviour
     void OnTriggerEnter2D()
     {
         manager.IncreaseSpareParts();
+        audioSource.PlayOneShot(sound, 2);
         this.gameObject.SetActive(false);
     }
 }
